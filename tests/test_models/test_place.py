26af9d71f24678be29f4efc33e1c589945fc2d75
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2023-09-11 23:18:55 +0200

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,55 +1,57 @@
 #!/usr/bin/python3
-
-"""Unittest for Place Class."""
-
+""" Contains unittests for Place class """
 import unittest
-
-from ...models.place import Place
-from ...models.city import City
-from ...models.user import User
-
-from ...models.base_model import BaseModel
+import os
+from models.base_model import BaseModel
+from models.place import Place
 
 
-class TestPlace(unittest.TestCase):
-    """Test cases Place class."""
+class TestPlaceClass(unittest.TestCase):
+    """ Tests Place class """
 
-    def test_instance(self):
-        """test instance."""
-        place = Place()
-        self.assertIsInstance(place, Place)
-
-    def test_is_class(self):
-        """test instance."""
-        place = Place()
-        self.assertEqual(str(type(place)),
-                         "<class 'models.place.Place'>")
-
-    def test_is_subclass(self):
-        """test is_subclass."""
-        place = Place()
-        self.assertTrue(issubclass(type(place), BaseModel))
-
-    def test_is_attr(self):
-        """test instance."""
-        city = City()
-        user = User()
-        place = Place()
-        place.user_id = user.id
-        place.city_id = city.id
-        self.assertIsNotNone(place.id)
-        self.assertEqual(place.user_id, user.id)
-        self.assertEqual(place.city_id, city.id)
-        self.assertEqual(place.name, "")
-        self.assertEqual(place.description, "")
-        self.assertEqual(place.number_rooms, 0)
-        self.assertEqual(place.number_bathrooms, 0)
-        self.assertEqual(place.max_guest, 0)
-        self.assertEqual(place.price_by_night, 0)
-        self.assertEqual(place.latitude, 0)
-        self.assertEqual(place.longitude, 0)
-        self.assertEqual(place.amenity_ids, [])
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_class(self):
+        """ tests class instantiation and class attributes """
+        # create object instance of Place Class
+        obj = Place()
+        # check if object is an instance of Place and parent class
+        self.assertIsInstance(obj, Place)
+        self.assertIsInstance(obj, BaseModel)
+        # check if dictionaries contain all expected attributes
+        # __dict__ only contains set attributes so this checks if set
+        self.assertIn("id", obj.__dict__)
+        self.assertIn("created_at", obj.__dict__)
+        self.assertIn("updated_at", obj.__dict__)
+        self.assertIn("city_id", Place.__dict__)
+        self.assertIn("user_id", Place.__dict__)
+        self.assertIn("name", Place.__dict__)
+        self.assertIn("description", Place.__dict__)
+        self.assertIn("number_rooms", Place.__dict__)
+        self.assertIn("number_bathrooms", Place.__dict__)
+        self.assertIn("max_guest", Place.__dict__)
+        self.assertIn("price_by_night", Place.__dict__)
+        self.assertIn("latitude", Place.__dict__)
+        self.assertIn("longitude", Place.__dict__)
+        self.assertIn("amenity_ids", Place.__dict__)
+        # check if Place class attribute initialized correctly
+        self.assertEqual(Place.city_id, "")
+        self.assertEqual(Place.user_id, "")
+        self.assertEqual(Place.name, "")
+        self.assertEqual(Place.description, "")
+        self.assertEqual(Place.number_rooms, 0)
+        self.assertEqual(Place.number_bathrooms, 0)
+        self.assertEqual(Place.max_guest, 0)
+        self.assertEqual(Place.price_by_night, 0)
+        self.assertEqual(Place.latitude, 0.0)
+        self.assertEqual(Place.longitude, 0.0)
+        self.assertEqual(Place.amenity_ids, [])
+        self.assertEqual(obj.city_id, "")
+        self.assertEqual(obj.user_id, "")
+        self.assertEqual(obj.name, "")
+        self.assertEqual(obj.description, "")
+        self.assertEqual(obj.number_rooms, 0)
+        self.assertEqual(obj.number_bathrooms, 0)
+        self.assertEqual(obj.max_guest, 0)
+        self.assertEqual(obj.price_by_night, 0)
+        self.assertEqual(obj.latitude, 0.0)
+        self.assertEqual(obj.longitude, 0.0)
+        self.assertEqual(obj.amenity_ids, [])
