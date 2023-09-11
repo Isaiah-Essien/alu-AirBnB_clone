1685fde7031570c6bb57a7c7b54fa59c10a5320a
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2023-09-11 23:20:21 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-
-
-"""Unittest for State Class."""
-
+""" Contains unittests for State class """
 import unittest
-
-from ...models.state import State
-
-from ...models.base_model import BaseModel
-
-
-class TestState(unittest.TestCase):
-    """Test cases State class."""
-
-    def test_instance(self):
-        """test instance."""
-        state = State()
-        self.assertIsInstance(state, State)
-
-    def test_is_class(self):
-        """test instance."""
-        state = State()
-        self.assertEqual(str(type(state)),
-                         "<class 'models.state.State'>")
-
-    def test_is_subclass(self):
-        """test is_subclass."""
-        state = State()
-        self.assertTrue(issubclass(type(state), BaseModel))
-
-    def test_attr(self):
-        """test is_subclass."""
-        state = State()
-        self.assertIsNotNone(state.id)
-        self.assertEqual(state.name, "")
+import os
+from models.base_model import BaseModel
+from models.state import State
 
 
-if __name__ == "__main__":
-    unittest.main()
+class TestStateClass(unittest.TestCase):
+    """ Tests State class """
+
+    def test_class(self):
+        """ tests class instantiation and class attributes """
+        # create object instance of State Class
+        obj = State()
+        # check if object is an instance of State and parent class
+        self.assertIsInstance(obj, State)
+        self.assertIsInstance(obj, BaseModel)
+        # check if dictionaries contain all expected attributes
+        # __dict__ only contains set attributes so this checks if set
+        self.assertIn("id", obj.__dict__)
+        self.assertIn("created_at", obj.__dict__)
+        self.assertIn("updated_at", obj.__dict__)
+        self.assertIn("name", State.__dict__)
+        # check if State class attribute initialized correctly
+        self.assertEqual(State.name, "")
+        self.assertEqual(obj.name, "")
