dce86831e1328212d4983ff7e62b7106802d6264
Isaiah-Essien <i.essien@alustudent.com>
2023-09-11 21:25:54 +0200

    anotherone

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,24 +12,36 @@ from models.base_model import BaseModel
 class TestAmenity(unittest.TestCase):
     """Test cases for  Amenity class."""
 
-    def test_instance(self):
-        """test instance."""
+    def test_class(self):
+        """test instantiation and attributes"""
         amenity = Amenity()
         self.assertIsInstance(amenity, Amenity)
-
-    def test_is_class(self):
-        """test instance."""
-        amenity = Amenity()
-        self.assertEqual(str(type(amenity)),
-                         "<class 'models.amenity.Amenity'>")
-
-    def test_is_subclass(self):
-        """test is_subclass."""
-        amenity = Amenity()
-        self.assertIn(amenity, BaseModel)
-
-    def test_attr(self):
-        """test is_subclass."""
-        amenity = Amenity()
-        self.assertEqual(amenity.name, "")
+        self.assertIsInstance(amenity, BaseModel)
+        self.assertIn("id", amenity.__dict__)
+        self.assertIn("created_at", amenity.__dict__)
+        self.assertIn("updated_at", amenity.__dict__)
+        self.assertIn("name", Amenity.__dict__)
         self.assertEqual(Amenity.name, "")
+        self.assertEqual(amenity.name, "")
+
+    # def test_instance(self):
+    #     """test instance."""
+    #     amenity = Amenity()
+    #     self.assertIsInstance(amenity, Amenity)
+
+    # def test_is_class(self):
+    #     """test instance."""
+    #     amenity = Amenity()
+    #     self.assertEqual(str(type(amenity)),
+    #                      "<class 'models.amenity.Amenity'>")
+
+    # def test_is_subclass(self):
+    #     """test is_subclass."""
+    #     amenity = Amenity()
+    #     self.assertIn(amenity, BaseModel)
+
+    # def test_attr(self):
+    #     """test is_subclass."""
+    #     amenity = Amenity()
+    #     self.assertEqual(amenity.name, "")
+    #     self.assertEqual(Amenity.name, "")
